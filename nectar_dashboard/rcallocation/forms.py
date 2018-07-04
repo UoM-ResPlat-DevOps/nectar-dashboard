UoM-ResPlat-DevOps/nectar-dashboard

commit f26222d1ff4aced412546b1481ae9b10a7f6bb83

Allocations FOR codes: removed percs and FOR3 from form

nectar_dashboard/rcallocation/forms.py
@@ -38,9 +38,8 @@ class BaseAllocationForm(ModelForm):
         }
 
     groups = (
-        ('field_of_research_1', 'for_percentage_1'),
-        ('field_of_research_2', 'for_percentage_2'),
-        ('field_of_research_3', 'for_percentage_3'),
+        ('field_of_research_1',),
+        ('field_of_research_2',),
     )
 
     def __init__(self, **kwargs):
@@ -86,25 +85,11 @@ class BaseAllocationForm(ModelForm):
 
     def clean(self):
         cleaned_data = super(BaseAllocationForm, self).clean()
-        fors = []
-        for for_name, perc_name in self.groups:
-            perc = cleaned_data.get(perc_name)
-            FOR = cleaned_data.get(for_name)
-            if not FOR and not perc:
-                continue
-            if FOR and perc == 0:
-                raise FORValidationError(
-                    "Percentage for Field Of Research '%s' cannot be 0" % FOR)
 
-            if not FOR and perc > 0:
-                raise FORValidationError(
-                    "Percentage set for unspecified Field Of Research")
-            fors.append(perc)
-
-        for_sum = sum(fors)
-        if for_sum > 100:
+        field_of_research_1 = cleaned_data.get('field_of_research_1')
+        if field_of_research_1 is None:
             raise FORValidationError(
-                "Sum of Field Of Research percentages greater than 100")
+                "You must make a selection for the First Field of Research.")
 
         return cleaned_data
 

nectar_dashboard/rcallocation/templates/rcallocation/allocationrequest_edit.html
@@ -178,14 +178,13 @@
     </div>
     <div class="row">
       <div class="col-md-12">
-        <h2>Fields of Research (FOR)</h2>
+        <h2>Fields of Research</h2>
         <hr/>
         <span class="label label-info">Note:</span>
         {% autoescape off %}
-        Please select up to 3 Fields of Research (FOR)
-        codes describing your work, adding up to a total
-        of 100%. For more information on FOR codes please
-        refer to the following <a href="http://www.abs.gov.au/Ausstats/abs@.nsf/Latestproducts/6BB427AB9696C225CA2574180004463E?opendocument" target="_blank">website</a>
+        Select up to two Field of Research (FOR) codes describing your work
+        (minimum one). For more information on FOR codes please refer to the
+        <a href="http://www.abs.gov.au/Ausstats/abs@.nsf/Latestproducts/6BB427AB9696C225CA2574180004463E?opendocument" target="_blank">Australian Bureau of Statistics website</a>.
         {% endautoescape %}
         <br/>
         <br/>
@@ -202,7 +201,7 @@
         <div class="form-group {% if field.errors %}has-error{% endif %}">
           {% for field in group %}
             {% if forloop.first %}
-              <label>{{ field.label }}</label>
+              <label>{{ field.label }}{% if field.name == 'field_of_research_1' %} <span class="glyphicon glyphicon-asterisk text-danger">{% endif %}</span></label>
             {% endif %}
             {{ field }}
           {% endfor %}

commit 720541153b6e5808f30bd7b1c92e05d43ee8b6eb

Fix for bug introduced by f26222d (form validation failure)

nectar_dashboard/rcallocation/forms.py
@@ -23,7 +23,9 @@ class BaseAllocationForm(ModelForm):
         exclude = ('status', 'created_by', 'submit_date', 'approver_email',
                    'modified_time', 'parent_request',
                    'funding_national_percent', 'funding_node', 'provisioned',
-                   'project_id', 'notes', 'allocation_home')
+                   'project_id', 'notes', 'allocation_home', 'for_percentage_1',
+                   'for_percentage_2', 'field_of_research_3',
+                   'for_percentage_3',)
 
         widgets = {
             'start_date': TextInput(attrs={'class': 'datepicker2 col-md-12'}),

nectar_dashboard/rcallocation/models.py
@@ -135,6 +135,40 @@ class AllocationRequest(models.Model):
                 """
     )
 
+    FOR_CHOICES = for_choices.FOR_CHOICES
+    PERCENTAGE_CHOICES = (
+        (0, '0%'),
+        (10, '10%'),
+        (20, '20%'),
+        (30, '30%'),
+        (40, '40%'),
+        (50, '50%'),
+        (60, '60%'),
+        (70, '70%'),
+        (80, '80%'),
+        (90, '90%'),
+        (100, '100%'),
+    )
+
+    for_percentage_1 = models.IntegerField(
+        choices=PERCENTAGE_CHOICES, default=100, blank=True,
+        help_text="""The percentage""")
+
+    for_percentage_2 = models.IntegerField(
+        choices=PERCENTAGE_CHOICES, default=0, blank=True,
+        help_text="""The percentage""")
+
+    field_of_research_3 = models.CharField(
+        "Third Field Of Research",
+        choices=FOR_CHOICES,
+        blank=True,
+        null=True,
+        max_length=6
+    )
+
+    for_percentage_3 = models.IntegerField(
+        choices=PERCENTAGE_CHOICES, default=0, blank=True)
+
     # The ordering of the following fields are important, as it
     # governs the order they appear on the forms
     project_name = models.CharField(
@@ -311,21 +345,6 @@ class AllocationRequest(models.Model):
             ..."""
     )
 
-    FOR_CHOICES = for_choices.FOR_CHOICES
-    PERCENTAGE_CHOICES = (
-        (0, '0%'),
-        (10, '10%'),
-        (20, '20%'),
-        (30, '30%'),
-        (40, '40%'),
-        (50, '50%'),
-        (60, '60%'),
-        (70, '70%'),
-        (80, '80%'),
-        (90, '90%'),
-        (100, '100%'),
-    )
-
     field_of_research_1 = models.CharField(
         "First Field Of Research",
         choices=FOR_CHOICES,
@@ -334,10 +353,6 @@ class AllocationRequest(models.Model):
         max_length=6
     )
 
-    for_percentage_1 = models.IntegerField(
-        choices=PERCENTAGE_CHOICES, default=100,
-        help_text="""The percentage""")
-
     field_of_research_2 = models.CharField(
         "Second Field Of Research",
         choices=FOR_CHOICES,
@@ -345,21 +360,6 @@ class AllocationRequest(models.Model):
         null=True,
         max_length=6
     )
-
-    for_percentage_2 = models.IntegerField(
-        choices=PERCENTAGE_CHOICES, default=0,
-        help_text="""The percentage""")
-
-    field_of_research_3 = models.CharField(
-        "Third Field Of Research",
-        choices=FOR_CHOICES,
-        blank=True,
-        null=True,
-        max_length=6
-    )
-
-    for_percentage_3 = models.IntegerField(
-        choices=PERCENTAGE_CHOICES, default=0)
 
     nectar_support = models.CharField(
         """List any ANDS, Nectar, or RDS funded projects supporting this